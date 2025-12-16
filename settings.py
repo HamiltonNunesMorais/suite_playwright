1bd3195ecc8fc4cb9cdc357d6076282b17a58da8
Hamilton Nunes de Morais <89794902+HamiltonNunesMorais@users.noreply.github.com>
2025-12-16 11:06:52 -0300

    update configs

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
-BASE_URL = "https://demowebshop.tricentis.com/"
-EMAIL = "mitodroid2015@gmail.com"   # substitua pelo seu
-PASSWORD = "TestTosca2025@"          # substitua pelo seu
+import os
+from dotenv import load_dotenv
+
+# Carrega variáveis do arquivo .env
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL", "https://demowebshop.tricentis.com/")
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
